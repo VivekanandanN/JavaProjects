fd8d38fe573d370a95b26137580fb435d1fbfb74
VivekanandanN <Vivekn707@gmail.com>
2019-09-25 22:28:32 -0700

    R Program

---
+++ Prog1.R
@@ -0,0 +1,11 @@
+# Add two vectors
+X = c(1,2,3)
+Y = c(4,5,6)
+X+Y
+
+#Get input from user
+#my.name <- readline(prompt="Enter name: ")
+my.age <- readline(prompt="Enter age: ")
+my.age = as.integer(my.age)
+print(paste("Hi, next year you will be", my.age+1, "years old."))
+
